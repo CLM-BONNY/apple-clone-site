466a6cec1b3a3279531e74b46e7045f7a6fba921
minseon6371 <66056874+minseon6371@users.noreply.github.com>
2021-09-06 05:46:57 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 (() => {
+    let yOffset = 0; // window.pageYOffseteotls 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 scroll-section들의 높이값의 합
+    let currentScene = 0; // 현재 활성화된(눈앞에 보고 있는) 씬(scroll-section)
+
     const sceneInfo = [
         {
             // 0
@@ -44,9 +48,26 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
         }
+    }
 
+    function scrollLoop() {
+        prevScrollHeight = 0;
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+        }
+        if(yOffset < prevScrollHeight) {
+            if(currentScene === 0) return; // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
+            currentScene--;
+        }
     }
 
     window.addEventListener('resize', setLayout);
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    });
     setLayout();
 })();
